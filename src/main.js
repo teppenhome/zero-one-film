7e8dc1a7a0dc901f89a9e00c845392a8d07ecc7a
teppenhome <teppen.create@gmail.com>
2026-02-07 00:14:46 +0900

    update styles and background

--- src/main.js
+++ src/main.js
@@ -10,7 +10,6 @@ import { initAboutVideo } from "./js/modules/aboutVideo";
 import { initVideoPlayer } from "./js/modules/videoPlayer";
 import { initAboutBgtext } from "./js/modules/aboutBgtext";
 import { initReveal } from "./js/modules/reveal";
-import { initPhilosophyParallax } from "./js/modules/parallax.js";
 import { initHeroVideoLoader } from "./js/modules/heroVideoLoader";
 
 // =====================================
@@ -29,7 +28,6 @@ function runInit() {
     rootMargin: "0px 0px -5% 0px",
     threshold: 0.25,
   });
-  if (typeof initPhilosophyParallax === "function") initPhilosophyParallax();
 }
 
 // DOM ready
